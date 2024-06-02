eaf66c8889f325ab96505a2570c76f7590f7fdb8
Daniel Bowman <daniel@danielbowman.co.uk>
2024-06-02 12:29:50 +0100

    Initial commit

---
+++ lua/quick-drafts/init.lua
@@ -0,0 +1,34 @@
+local M = {}
+
+function DraftDatedFile(path)
+	local date = os.date("%Y-%m-%d %H:%M")
+	return drafts_root .. path .. date .. ".md"
+end
+
+function DraftNew()
+	vim.cmd("e" .. DraftDatedFile(""))
+end
+
+function DraftStandup()
+	vim.cmd("e" .. DraftDatedFile("standup/"))
+end
+
+vim.api.nvim_create_autocmd("BufNewFile", {
+	pattern = "*/standup/*.md",
+	callback = function(_)
+		vim.cmd("0r ~/dotvim/templates/standup.skeleton")
+	end,
+	desc = "Template standup",
+})
+
+M.draft_new = DraftNew
+M.draft_standup = DraftStandup
+
+function M.setup(opts)
+	opts = opts or {}
+	drafts_root = opts.root
+	vim.keymap.set("n", "<Leader>Dn", "<cmd>lua DraftNew()<CR>", { silent = true })
+	vim.keymap.set("n", "<Leader>Ds", "<cmd>lua DraftStandup()<CR>", { silent = true })
+	vim.keymap.set("n", "<Leader>Dt", "<cmd>10split " .. drafts_root .. "tasks.md<CR>", { silent = true })
+end
+return M
